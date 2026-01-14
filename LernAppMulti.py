68d5cbaef6dd05d7920010795692ef579cc59d3f
DST81 <148617124+DST81@users.noreply.github.com>
2026-01-14 21:23:32 +0100

    Add 'Medizin Schwierige Fragen' to JSON mappings

--- LernAppMulti.py
+++ LernAppMulti.py
@@ -17,6 +17,7 @@ verfügbare_fächer = {
     "Medizin Tag 3": "Medizin_Tag3.json",
     "Medizin Tag 4": "Medizin_Tag4.json",
     "Medizin Tag 5": "Medizin_Tag5.json",
+    "Medizin Schwierige Fragen": "Medizin_AlleTage_schwierigereFragen.json",
     "Einführung Banken": "mc_bank_Kapitel1.json",
     "Doppelte Buchführung": "mc_bank_Kapitel2.json",
     "Modellierung von Banken":"mc_bank_Kapitel3.json",
