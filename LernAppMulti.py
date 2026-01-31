551a1240ae104fca2c9fb201dfc0c9b44e1524bc
DST81 <148617124+DST81@users.noreply.github.com>
2026-01-31 14:35:14 +0100

    Add 'RL Monte Carlo' to available subjects

--- LernAppMulti.py
+++ LernAppMulti.py
@@ -12,6 +12,7 @@ st.sidebar.image("helfer.png", caption="Hallo, ich bin dein Lernassistent", use_
 st.sidebar.title("📚 Fachauswahl")
    
 verfügbare_fächer = {
+    "RL Monte Carlo": "rl/mc.json",
     "RL Dynamic Programming":"rl/dp.json",
     "RL Konzepte": "rl/konzeptfragen.json",
     "RL Grundlagen":"rl/grundlagen.json",
